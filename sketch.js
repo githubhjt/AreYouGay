292c05bfed72afaa96154d8a15a8418392f3e7df
jntk.hong <jthonga2@gmail.com>
2024-04-02 14:36:18 +0900

    sketch.js 파일로 옮겨오기

--- sketch.js
+++ sketch.js
@@ -1,8 +1,24 @@
 function setup() {
-  createCanvas(400, 400);
+  // 캔버스 생성을 방지
+  noCanvas();
+
+  // 입력 필드 생성
+  let nameInput = createInput('').attribute('placeholder', 'First Name');
+  let surnameInput = createInput('').attribute('placeholder', 'Last Name');
+  let phoneNumberInput = createInput('').attribute('placeholder', 'Phone Number');
+  
+  // 버튼 생성 및 클릭 이벤트 설정
+  let submitBtn = createButton('Done');
+  submitBtn.mousePressed(() => {
+    console.log("First Name: " + nameInput.value());
+    console.log("Last Name: " + surnameInput.value());
+    console.log("Phone Number: " + phoneNumberInput.value());
+  });
+
+  // 스타일링을 위한 클래스 추가 (옵션)
+  nameInput.parent('input-group');
+  surnameInput.parent('input-group');
+  phoneNumberInput.parent('input-group');
+  submitBtn.parent('centered-content');
 }
 
-function draw() {
-  background(220);
-  square(100,100,30,30);
-}
