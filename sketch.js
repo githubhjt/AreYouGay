781d773fd798fc94361dbfde651e045a773cd371
jntk.hong <jthonga2@gmail.com>
2024-04-05 22:39:04 +0900

    전화번호 입력하는 칸들끼리 좌우로 나란히 정렬시키기

--- sketch.js
+++ sketch.js
@@ -4,38 +4,31 @@ function setup() {
   // 중앙 정렬을 위한 컨테이너 생성
   let centeredContent = createDiv('').addClass('centered-content');
 
-  // 입력 필드 그룹 생성
-  let inputGroup1 = createDiv('').addClass('input-group');
-  let inputGroup2 = createDiv('').addClass('input-group');
-  let inputGroup3 = createDiv('').addClass('input-group'); // 추가된 부분
-
-  // 이름 입력 필드 생성
+  // 이름 입력 필드 그룹 생성
+  let nameInputGroup = createDiv('').addClass('input-group');
   let nameInput = createInput('').attribute('placeholder', '이름');
   let surnameInput = createInput('').attribute('placeholder', '성');
+  nameInput.parent(nameInputGroup);
+  surnameInput.parent(nameInputGroup);
 
-  // 수정된 부분: 휴대폰 번호 입력 필드를 세 개로 분할하여 생성
-  let phoneNumberInput1 = createInput('').attribute('maxlength', '3').attribute('placeholder', '000');
-  let phoneNumberInput2 = createInput('').attribute('maxlength', '4').attribute('placeholder', '');
-  let phoneNumberInput3 = createInput('').attribute('maxlength', '4').attribute('placeholder', '0000');
-
-  // 입력 필드를 그룹에 추가
-  nameInput.parent(inputGroup1);
-  surnameInput.parent(inputGroup1);
-  phoneNumberInput1.parent(inputGroup2);
-  phoneNumberInput2.parent(inputGroup2);
-  phoneNumberInput3.parent(inputGroup3); // 추가된 부분
+  // 전화번호 입력 필드 그룹 생성
+  let phoneInputGroup = createDiv('').addClass('input-group');
+  let phoneNumberInput1 = createInput('').attribute('maxlength', '3').attribute('placeholder', '000').addClass('phone-input');
+  let phoneNumberInput2 = createInput('').attribute('maxlength', '4').attribute('placeholder', '').addClass('phone-input');
+  let phoneNumberInput3 = createInput('').attribute('maxlength', '4').attribute('placeholder', '0000').addClass('phone-input');
+  phoneNumberInput1.parent(phoneInputGroup);
+  phoneNumberInput2.parent(phoneInputGroup);
+  phoneNumberInput3.parent(phoneInputGroup);
 
   // 그룹을 centeredContent에 추가
-  inputGroup1.parent(centeredContent);
-  inputGroup2.parent(centeredContent);
-  inputGroup3.parent(centeredContent); // 추가된 부분
+  nameInputGroup.parent(centeredContent);
+  phoneInputGroup.parent(centeredContent);
 
   // 버튼 생성 및 이벤트 리스너 설정
   let submitBtn = createButton('완료');
   submitBtn.parent(centeredContent);
   submitBtn.mousePressed(() => {
-    console.log("이름: " + nameInput.value());
-    console.log("성: " + surnameInput.value());
+    console.log("이름: " + nameInput.value() + " " + surnameInput.value());
     console.log("휴대폰 번호: " + phoneNumberInput1.value() + phoneNumberInput2.value() + phoneNumberInput3.value());
   });
 
