f281705bfb3b616847f8f253d419a88371912db5
jntk.hong <jthonga2@gmail.com>
2024-04-05 22:25:10 +0900

    First Name과 Last Name끼리 나란히 정렬하기

--- sketch.js
+++ sketch.js
@@ -1,20 +1,32 @@
 function setup() {
-  // 캔버스 생성을 방지
-  noCanvas();
+  noCanvas(); // 캔버스 생성 방지
 
-  // 'centered-content' 요소 생성 및 body에 추가
+  // 중앙 정렬을 위한 컨테이너 생성
   let centeredContent = createDiv('').addClass('centered-content');
-  
-  // 'input-group' 요소 생성 및 'centered-content'에 추가
-  let inputGroup = createDiv('').addClass('input-group').parent(centeredContent);
 
-  // 입력 필드 생성 및 'input-group'에 추가
-  let nameInput = createInput('').attribute('placeholder', 'First Name').parent(inputGroup);
-  let surnameInput = createInput('').attribute('placeholder', 'Last Name').parent(inputGroup);
-  let phoneNumberInput = createInput('').attribute('placeholder', 'Phone Number').parent(inputGroup);
-  
-  // 버튼 생성 및 'centered-content'에 추가, 클릭 이벤트 설정
-  let submitBtn = createButton('Done').parent(centeredContent);
+  // 입력 필드 그룹 생성
+  let inputGroup1 = createDiv('').addClass('input-group');
+  let inputGroup2 = createDiv('').addClass('input-group');
+
+  // 이름 입력 필드 생성
+  let nameInput = createInput('').attribute('placeholder', 'First Name');
+  let surnameInput = createInput('').attribute('placeholder', 'Last Name');
+
+  // 전화번호 입력 필드 생성
+  let phoneNumberInput = createInput('').attribute('placeholder', 'Phone Number');
+
+  // 입력 필드를 그룹에 추가
+  nameInput.parent(inputGroup1);
+  surnameInput.parent(inputGroup1);
+  phoneNumberInput.parent(inputGroup2);
+
+  // 그룹을 centeredContent에 추가
+  inputGroup1.parent(centeredContent);
+  inputGroup2.parent(centeredContent);
+
+  // 버튼 생성 및 이벤트 리스너 설정
+  let submitBtn = createButton('Done');
+  submitBtn.parent(centeredContent);
   submitBtn.mousePressed(() => {
     console.log("First Name: " + nameInput.value());
     console.log("Last Name: " + surnameInput.value());
