5c9d1e763f9dc59363bbe8696f2a1fa34c011253
jntk.hong <jthonga2@gmail.com>
2024-04-05 21:50:13 +0900

    DOM을 이용한 코딩 전환에서 생기는 오류 제거하기

--- sketch.js
+++ sketch.js
@@ -2,23 +2,22 @@ function setup() {
   // 캔버스 생성을 방지
   noCanvas();
 
-  // 입력 필드 생성
-  let nameInput = createInput('').attribute('placeholder', 'First Name');
-  let surnameInput = createInput('').attribute('placeholder', 'Last Name');
-  let phoneNumberInput = createInput('').attribute('placeholder', 'Phone Number');
+  // 'centered-content' 요소 생성 및 body에 추가
+  let centeredContent = createDiv('').addClass('centered-content');
   
-  // 버튼 생성 및 클릭 이벤트 설정
-  let submitBtn = createButton('Done');
+  // 'input-group' 요소 생성 및 'centered-content'에 추가
+  let inputGroup = createDiv('').addClass('input-group').parent(centeredContent);
+
+  // 입력 필드 생성 및 'input-group'에 추가
+  let nameInput = createInput('').attribute('placeholder', 'First Name').parent(inputGroup);
+  let surnameInput = createInput('').attribute('placeholder', 'Last Name').parent(inputGroup);
+  let phoneNumberInput = createInput('').attribute('placeholder', 'Phone Number').parent(inputGroup);
+  
+  // 버튼 생성 및 'centered-content'에 추가, 클릭 이벤트 설정
+  let submitBtn = createButton('Done').parent(centeredContent);
   submitBtn.mousePressed(() => {
     console.log("First Name: " + nameInput.value());
     console.log("Last Name: " + surnameInput.value());
     console.log("Phone Number: " + phoneNumberInput.value());
   });
-
-  // 스타일링을 위한 클래스 추가 (옵션)
-  nameInput.parent('input-group');
-  surnameInput.parent('input-group');
-  phoneNumberInput.parent('input-group');
-  submitBtn.parent('centered-content');
 }
-
